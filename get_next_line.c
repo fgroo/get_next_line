10525c707d439d05498a1e4ce95d51fb605610d1
fgroo <faris.goerlich@gmail.com>
2025-11-13 12:34:42 +0100

    moulinette fix, remove of BUF

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fgroo <student@42.eu>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 17:10:58 by fgroo             #+#    #+#             */
-/*   Updated: 2025/11/10 23:48:09 by fgroo            ###   ########.fr       */
+/*   Updated: 2025/11/13 12:31:51 by fgroo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@ char	*get_next_line(int fd)
 	t_tmp			tmp;
 
 	tmp = (t_tmp){0, -1, NULL};
-	if (fd < 0 || !BUF || fd >= 1024 || (read(fd, 0, 0) < 0))
-		return (v.rd[fd] = 0, NULL);
+	if (fd < 0 || !BUFFER_SIZE || fd >= 1024 || (read(fd, 0, 0) < 0))
+		return (v.r[fd] = 0, NULL);
 	while (1)
 	{
-		if (v.pos[fd] >= v.rd[fd] && v.rd[fd] != -1)
-			(free(0), v.pos[fd] = 0, v.rd[fd] = read(fd, v.tmp[fd], BUF));
-		if (!v.rd[fd])
+		if (v.p[fd] >= v.r[fd] && v.r[fd] != -1)
+			(free(0), v.p[fd] = 0, v.r[fd] = read(fd, v.tmp[fd], BUFFER_SIZE));
+		if (!v.r[fd])
 			break ;
-		v.buf[fd][tmp.len++] = v.tmp[fd][v.pos[fd]++];
+		v.buf[fd][tmp.len++] = v.tmp[fd][v.p[fd]++];
 		if (v.buf[fd][tmp.len - 1] == '\n')
 			break ;
 	}
-	if (v.rd[fd] != -1 && tmp.len > 0)
+	if (v.r[fd] != -1 && tmp.len > 0)
 	{
 		tmp.res = malloc(tmp.len + 1);
 		if (!tmp.res)
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: fgroo <student@42.eu>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 17:11:04 by fgroo             #+#    #+#             */
-/*   Updated: 2025/11/10 23:46:54 by fgroo            ###   ########.fr       */
+/*   Updated: 2025/11/13 12:31:58 by fgroo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@
 #  define BUFFER_SIZE 100000
 # endif
 
-# ifndef BUF
-#  define BUF BUFFER_SIZE
-# endif
-
 # include <unistd.h>
 # include <stdlib.h>
 
@@ -28,8 +24,8 @@ typedef struct s_vars
 {
 	char	buf[1024][100000];
 	char	tmp[1024][100000];
-	ssize_t	pos[1024];
-	ssize_t	rd[1024];
+	ssize_t	p[1024];
+	ssize_t	r[1024];
 	char	*res;
 }	t_vars;
 
